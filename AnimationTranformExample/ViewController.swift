59e6987aa772b6a1f13b961097db38a11cf1013d
Felipe Del Campo Gomes dos Santos <felipegs@ciandt.com>
2017-12-19 11:14:23 -0200

    Initial Commit

--- AnimationTranformExample/ViewController.swift
+++ AnimationTranformExample/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func viewCardTouched(_ sender: UIButton) {
+        
+        // TODO: Animate here
+        
+    }
+    
+    
 }
 
